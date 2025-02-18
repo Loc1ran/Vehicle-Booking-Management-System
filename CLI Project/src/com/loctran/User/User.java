912b17e9d6ea7c2d75cb566b42af418ede7d8297
Loc1ran <loc.tntran@gmail.com>
2025-02-18 17:57:09 -0600

    Add files via upload

---
+++ CLI Project/src/com/loctran/User/User.java
@@ -0,0 +1,43 @@
+package com.loctran.User;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class User {
+    private String name;
+    private UUID id;
+
+    public User(UUID id, String name) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name) && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
