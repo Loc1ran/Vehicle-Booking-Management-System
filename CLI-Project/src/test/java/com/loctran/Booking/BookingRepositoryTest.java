293d6a617466870fcf0bccd8bb2ab71ccd3dbfff
Loc <loc.tntran@gmail.com>
2025-07-08 17:14:39 -0500

    Testing Booking Repository

---
+++ CLI-Project/src/test/java/com/loctran/Booking/BookingRepositoryTest.java
@@ -0,0 +1,80 @@
+package com.loctran.Booking;
+
+import com.loctran.Car.Brand;
+import com.loctran.Car.Car;
+import com.loctran.Car.CarRepository;
+import com.loctran.User.User;
+import com.loctran.User.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class BookingRepositoryTest {
+    @Autowired
+    BookingRepository bookingRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    CarRepository carRepository;
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAvailableCarsFromList() {
+        Car car1 = new Car("0193", new BigDecimal("12.34"), Brand.TESLA, true);
+        Car car2 = new Car("2105", new BigDecimal("32.34"), Brand.MERCEDES, false);
+        User user = new User("Loc");
+
+        car1 = carRepository.save(car1);
+        car2 = carRepository.save(car2);
+        user = userRepository.save(user);
+
+        Booking booking = new Booking(
+                car1, user
+        );
+
+        bookingRepository.save(booking);
+
+        List<Car> cars = carRepository.findAll();
+        List<Car> actual = bookingRepository.findAvailableCarsFromList(cars);
+
+        assertThat(actual).isNotEmpty().containsExactly(car2);
+    }
+
+    @Test
+    void findBookingsByUserId() {
+        Car car1 = new Car("1111", new BigDecimal("12.34"), Brand.TESLA, true);
+        Car car2 = new Car("2222", new BigDecimal("32.34"), Brand.MERCEDES, false);
+        User user = new User("Amanda");
+
+        car1 = carRepository.save(car1);
+        car2 = carRepository.save(car2);
+        user = userRepository.save(user);
+
+        Booking booking1 = new Booking(
+                car1, user
+        );
+
+        Booking booking2 = new Booking(car2, user);
+
+        bookingRepository.save(booking1);
+        bookingRepository.save(booking2);
+
+        List<Booking> actual =  bookingRepository.findBookingsByUserId(user.getId());
+
+        assertThat(actual).isNotEmpty().containsExactly(booking1, booking2);
+    }
+}
