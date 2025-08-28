b80d53ca3df4f3dd339ecb153ac45717c72654af
Loc <loc.tntran@gmail.com>
2025-08-28 18:34:25 -0500

    Fixing github.sha for slack message and add counter to pingpongController

--- backend/src/main/java/com/loctran/PingPongController.java
+++ backend/src/main/java/com/loctran/PingPongController.java
@@ -5,13 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingPongController {
+    private static int COUNTER = 0;
     record PingPong(String result){
 
     }
 
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("Ping");
+        return new PingPong("Pong: " + ++COUNTER);
     }
 
 
