d5ed5eae11ae9d2c607c15751fe9f35c273ab34d
Loc <loc.tntran@gmail.com>
2025-09-21 20:39:56 -0500

    allowedHost

--- frontend/my-react-app/vite.config.js
+++ frontend/my-react-app/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [process.env.VITE_API_BASE_URL]
+  }
 })
